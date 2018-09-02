f3d13a7646f46df086d6a5c29860b69dd483b55e
Steven Mertens <steven.mertens@catay.be>
2018-09-02 22:44:14 +0200

    Puppet task puppet
    * Remove obsolete code
    * Make the code RuboCop compliant
    * Add a proper Puppet disable message
    * Return the Puppet status in JSON format

--- puppet/tasks/init.rb
+++ puppet/tasks/init.rb
@@ -7,74 +7,88 @@ require 'puppet'
 # classes
 # code based on MCollective Puppet Agent.
 # https://github.com/puppetlabs/mcollective-puppet-agent
-
 class PuppetAgentMgr
-
   def initialize(config_file = nil)
-    unless Puppet.settings.app_defaults_initialized?
+    # set puppet agent run mode to agent
+    Puppet.settings.preferred_run_mode = :agent
 
-      # set puppet agent run mode to agent
-      Puppet.settings.preferred_run_mode = :agent
+    # use custom config if provided, if not use default
+    args = []
+    (args << "--config=#{config_file}") if config_file
 
-      # use custom config if provided, if not use default
-      args = []
-      (args << "--config=%s" % config_file) if config_file
-
-      Puppet.settings.initialize_global_settings(args)
-      Puppet.settings.initialize_app_defaults(
-        Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
-
-    end
+    Puppet.settings.initialize_global_settings(args)
+    Puppet.settings.initialize_app_defaults(
+      Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode)
+    )
   end
-  
+
   def status
     status = {
-        :disabled => disabled?,
-        :enabled => !disabled?,
-        :applying => applying?
+      disabled: disabled?,
+      enabled: !disabled?,
+      applying: applying?,
+      disable_message: disable_message
     }
     status
   end
 
   # enable the puppet agent
   def enable!
-      File.unlink(Puppet[:agent_disabled_lockfile]) if disabled?
+    File.unlink(Puppet[:agent_disabled_lockfile]) if disabled?
+    status
   end
 
   # disable the puppet agent
-  def disable!(msg=nil)
-    msg ||= "Disabled by task ## on date"
-    File.write(Puppet[:agent_disabled_lockfile], JSON.dump(:disabled_message => msg)) unless disabled?
+  def disable!(msg = nil)
+    if enabled?
+      msg ||= "Disabled by Puppet task on #{Time.now.strftime('%c')}"
+      File.write(Puppet[:agent_disabled_lockfile],
+                 JSON.dump(disabled_message: msg))
+    end
+
+    status
+  end
+
+  # get disable message
+  def disable_message
+    msg = if disabled?
+            JSON.parse(File.read(Puppet[:agent_disabled_lockfile]))
+          else
+            { disabled_message: nil }
+          end
+
+    msg['disabled_message']
+  end
+
+  # check if puppet agent is enabled
+  def enabled?
+    !disabled?
   end
 
   # check if puppet agent is disabled
   def disabled?
-    return File.exist?(Puppet[:agent_disabled_lockfile])
+    File.exist?(Puppet[:agent_disabled_lockfile])
   end
 
   # check if puppet agent run is in progress
   def applying?
     return false unless File.exist?(Puppet[:agent_catalog_run_lockfile])
-    return true
+    true
   end
-
 end
 
 # functions
 
 def puppet_agent(action)
   p = PuppetAgentMgr.new
-  stdout = ""
 
   case action
   when 'status'
     stdout = p.status
   when 'disable'
-    p.disable!
-    stdout = "disabled"
+    stdout = p.disable!
   when 'enable'
-    p.enable!
-    stdout = "enabled"
+    stdout = p.enable!
   end
 
   { status: stdout }
